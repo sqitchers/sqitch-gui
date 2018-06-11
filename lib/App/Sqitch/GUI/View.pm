5687ba32f6f7c73aa0e25133043688c5a470e829
Stefan Suciu <stefbv70@gmail.com>
2018-06-11 19:00:36 +0300

    Fix the fix, add missing method

--- lib/App/Sqitch/GUI/View.pm
+++ lib/App/Sqitch/GUI/View.pm
@@ -218,7 +218,7 @@ sub BUILD {
     }
     $tb->set_initial_mode( \@toolbars );
     $self->frame->SetToolBar($tb);
-    $tb = $self->GetToolBar;                 # is this really needed?
+    $tb = $self->frame->GetToolBar;          # is this really needed?
     $tb->Realize;
 
     $self->main_sizer->Add( $self->left->panel,  1, wxEXPAND | wxALL, 0 );
